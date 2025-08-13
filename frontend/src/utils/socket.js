6f30cc8ab5c63984827de0c85a1a32da75cb4765
Harinarayan S <harinarayansubu@gmail.com>
2025-08-13 13:20:35 +0530

    Update socket URL to production backend

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-const SOCKET_URL = "http://localhost:4000";
+const SOCKET_URL = "https://echo-eyes-backend.onrender.com";
 const socket = io(SOCKET_URL, { transports: ["websocket"] });
 export default socket;
